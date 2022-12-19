894a8d194777159790725b1cf1ba0aa7342cf3d6
Lakshan099 <lakshan.rsandanayaka@gmail.com>
2022-12-19 12:10:31 +0530

    final check

--- Challange_3.java
+++ Challange_3.java
@@ -5,8 +5,8 @@ public class Challange_3 {
         int size = 7;
         int arr[] = { 47, 84, 75, 21, 14, 14, 79 };
 
-        //System.out.println("Median value is: " + findaMedian(arr, size));
-        //System.out.println("Max value is: " + findaMax(arr, size));
+        System.out.println("Median value is: " + findaMedian(arr, size));
+        System.out.println("Max value is: " + findaMax(arr, size));
         System.out.println("Mode value is: " + findMode(arr, size));
 
     }
